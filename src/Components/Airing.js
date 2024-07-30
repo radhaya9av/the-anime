7437c2108054bc0ebf2a13cba87918da6d720892
radhaya9av <radhaya9av@gmail.com>
2024-07-30 10:06:02 +0530

    airingComponent

---
+++ src/Components/Airing.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useGlobalContext } from '../context/global'
+import Sidebar from './Sidebar'
+
+function Airing({rendered}) {
+    const {airingAnime ,isSearch, searchResults} = useGlobalContext()
+
+    const conditionalRender = () => {
+        if(!isSearch && rendered === 'airing'){
+            return airingAnime?.map((anime) => {
+                return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                    <img src={anime.images.jpg.large_image_url} alt="" />
+                </Link>
+            })
+        }else{
+            return searchResults?.map((anime) => {
+                return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                    <img src={anime.images.jpg.large_image_url} alt="" />
+                </Link>
+            })
+        }
+    }
+
+    return (
+        <PopularStyled>
+            <div className="airing-anime">
+                {conditionalRender()}
+            </div>
+            <Sidebar />
+        </PopularStyled>
+    )
+}
+
+export default Airing
