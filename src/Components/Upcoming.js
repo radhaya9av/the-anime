ac868ba5ba50b368c62e95f64b76872b8cf4905f
radhaya9av <radhaya9av@gmail.com>
2024-07-30 10:07:34 +0530

    upcomingComponent

---
+++ src/Components/Upcoming.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useGlobalContext } from '../context/global'
+import Sidebar from './Sidebar'
+
+function Upcoming({rendered}) {
+    const {upcomingAnime ,isSearch, searchResults} = useGlobalContext()
+
+    const conditionalRender = () => {
+        if(!isSearch && rendered === 'upcoming'){
+            return upcomingAnime?.map((anime) => {
+                return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                    <img src={anime.images.jpg.large_image_url} alt="" />
+                </Link>
+            })
+        }else{
+            return searchResults?.map((anime) => {
+                return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                    <img src={anime.images.jpg.large_image_url} alt="" />
+                </Link>
+            })
+        }
+    }
+
+    return (
+        <PopularStyled>
+            <div className="upcoming-anime">
+                {conditionalRender()}
+            </div>
+            <Sidebar />
+        </PopularStyled>
+    )
+}
+
+export default Upcoming
