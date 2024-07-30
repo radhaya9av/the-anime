991977ffe05433e79b5c5d65e744e7378cf79bae
radhaya9av <radhaya9av@gmail.com>
2024-07-30 10:03:23 +0530

    sidebarComponent

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { useGlobalContext } from '../context/global';
+
+
+function Sidebar() {
+    const {popularAnime} = useGlobalContext()
+
+    const sorted = popularAnime?.sort((a,b) => {
+        return b.score - a.score
+    })
+
+    return (
+        <SidebarStyled>
+            <h3>Top 5 Popular</h3>
+            <div className="anime">
+                {sorted?.slice(0,5).map((anime) => {
+                    return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                        <img src={anime.images.jpg.large_image_url} alt="" />
+                        <h5>
+                            {anime.title}
+                        </h5>
+                    </Link>
+                })}
+            </div>
+        </SidebarStyled>
+    )
+}
+
+export default Sidebar
