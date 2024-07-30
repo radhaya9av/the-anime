404283ecc25e4fa1cda6ad7b082ab30e94207dc8
radhaya9av <radhaya9av@gmail.com>
2024-07-30 10:04:02 +0530

    sidebarStyling

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import styled from 'styled-components'
 import { useGlobalContext } from '../context/global';
 
 
@@ -26,5 +27,34 @@ function Sidebar() {
         </SidebarStyled>
     )
 }
+
+const SidebarStyled = styled.div`
+    margin-top: 2rem;
+    background-color: #fff;
+    border-top: 5px solid #e5e7eb;
+    padding-right: 5rem;
+    padding-left: 2rem;
+    padding-top: 2rem;
+    .anime{
+        display: flex;
+        flex-direction: column;
+        width: 150px;
+        img{
+            width: 100%;
+            border-radius: 5px;
+            border: 5px solid #e5e7eb;
+        }
+        a{
+            margin-top: 1rem;
+            display: flex;
+            flex-direction: column;
+            gap: .4rem;
+            color: #27AE60;
+            h4{
+                font-size: 1.1rem;
+            }
+        }
+    }
+`;
 
 export default Sidebar
