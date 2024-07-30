18437d3e1c7e4e1ebda882b5c9493b79c6e33b5c
radhaya9av <radhaya9av@gmail.com>
2024-07-30 10:11:53 +0530

    homePageStyling

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useGlobalContext } from '../context/global'
 import Popular from './Popular'
+import styled from 'styled-components'
 import Upcoming from './Upcoming'
 import Airing from './Airing'
 
@@ -69,5 +70,69 @@ function Homepage() {
         </HomepageStyled >
     )
 }
+
+const HomepageStyled = styled.div`
+    background-color: #EDEDED;
+    header{
+        padding: 2rem 5rem;
+        width: 60%;
+        margin: 0 auto;
+        transition: all .4s ease-in-out;
+        @media screen and (max-width:1530px){
+            width: 95%;
+        }
+        .logo{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin-bottom: 2rem;
+        }
+        .search-container{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            gap: 1rem;
+            button{
+                display: flex;
+                align-items: center;
+                gap: .5rem;
+                padding: .7rem 1.5rem;
+                outline: none;
+                border-radius: 30px;
+                font-size: 1.2rem;
+                background-color: #fff;
+                cursor: pointer;
+                transition: all .4s ease-in-out;
+                font-family: inherit;
+                border: 5px solid #e5e7eb;
+            }
+            form{
+                position: relative;
+                width: 100%;
+                .input-control{
+                    position: relative;
+                    transition: all .4s ease-in-out;
+                }
+                .input-control input{
+                    width: 100%;
+                    padding:.7rem 1rem;
+                    border: none;
+                    outline: none;
+                    border-radius: 30px;
+                    font-size: 1.2rem;
+                    background-color: #fff;
+                    border: 5px solid #e5e7eb;
+                    transition: all .4s ease-in-out;
+                }
+                .input-control button{
+                    position: absolute;
+                    right: 0;
+                    top: 50%;
+                    transform: translateY(-50%);
+                }
+            }
+        }
+    }
+`
 
 export default Homepage
