0fe5213d612f98e04bbc644ea05e034f9a47912b
radhaya9av <radhaya9av@gmail.com>
2024-07-30 10:10:03 +0530

    popularComponent

---
+++ src/Components/Popular.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useGlobalContext } from '../context/global'
+import Sidebar from './Sidebar'
+
+function Popular({rendered}) {
+    const {popularAnime,isSearch, searchResults} = useGlobalContext()
+
+    const conditionalRender = () => {
+        if(!isSearch && rendered === 'popular'){
+            return popularAnime?.map((anime) => {
+                return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                    <img src={anime.images.jpg.large_image_url} alt="" />
+                </Link>
+            })
+        }else{
+            return searchResults?.map((anime) => {
+                return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                    <img src={anime.images.jpg.large_image_url} alt="" />
+                </Link>
+            })
+        }
+    }
+
+    return (
+        <PopularStyled>
+            <div className="popular-anime">
+                {conditionalRender()}
+            </div>
+            <Sidebar />
+        </PopularStyled>
+    )
+}
+
+export default Popular
