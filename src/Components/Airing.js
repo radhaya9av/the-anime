2f62b2efca7b7fd2409cefb6babbb0c24c22a004
radhaya9av <radhaya9av@gmail.com>
2024-07-30 10:06:29 +0530

    airingStyling

--- src/Components/Airing.js
+++ src/Components/Airing.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useGlobalContext } from '../context/global'
+import styled from 'styled-components'
 import Sidebar from './Sidebar'
 
 function Airing({rendered}) {
@@ -31,5 +32,33 @@ function Airing({rendered}) {
         </PopularStyled>
     )
 }
+
+const PopularStyled = styled.div`
+    display: flex;
+    .airing-anime{
+        margin-top: 2rem;
+        padding-top: 2rem;
+        padding-bottom: 2rem;
+        padding-left: 5rem;
+        padding-right: 0;
+        width: 100%;
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+        grid-gap: 2rem;
+        background-color: #fff;
+        border-top: 5px solid #e5e7eb;
+        a{
+            height: 500px;
+            border-radius: 7px;
+            border: 5px solid #e5e7eb;
+        }
+        a img{
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            border-radius: 5px;
+        }
+    }
+`;
 
 export default Airing
